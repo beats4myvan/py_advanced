50cbbeef96334c7d57630c888d7c2cb8c24da9d8
beats4myvan <georgidimitrov90@yahoo.bg>
2021-01-17 10:49:46 +0200

    x250 fast food

--- stacks_queues/2.3.fast_food.py
+++ stacks_queues/2.3.fast_food.py
@@ -1,6 +1,13 @@
 from collections import deque
+
 quantity_of_food = int(input())
-q_of_order = int(input())
+q_of_order = deque(map(int, input().split()))
+print(max(q_of_order))
 
-orders = deque()
+while q_of_order and q_of_order[0] <= quantity_of_food:
+    quantity_of_food -= q_of_order.popleft()
 
+if not q_of_order:
+    print(f'Orders complete')
+else:
+    print(f'Orders left: {" ".join(map(str, q_of_order))}')
